bfd0d4798af10337ddd0658f9b0863dfb8e8acfd
Dannhell <161614077+Dannhell@users.noreply.github.com>
2024-10-15 17:58:09 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  const quien = ["Mi Madre", "Un ladrón", "El paleta", "Mi gato"];
+  const accion = [
+    "ha roto",
+    "ha robado",
+    "ha infectado con virus",
+    "se ha llevado"
+  ];
+  const cosa = ["mi ordenador", "nuestra app", "mi móvil", "vuestro servidor"];
+  const cuando = [
+    "en la noche",
+    "cuando estábamos de vacaciones",
+    "en el horario laboral",
+    "a la hora de la siesta"
+  ];
+
+  const obtenerAleatorio = array =>
+    array[Math.floor(Math.random() * array.length)];
+
+  const excusa = `${obtenerAleatorio(quien)} ${obtenerAleatorio(
+    accion
+  )} ${obtenerAleatorio(cosa)} ${obtenerAleatorio(cuando)}`;
+
+  console.log(excusa);
+  document.getElementById("excusa").innerHTML = excusa;
   console.log("Hello Rigo from the console!");
 };
